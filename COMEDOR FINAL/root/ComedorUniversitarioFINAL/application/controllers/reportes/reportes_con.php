9568e3a52feeef2b40b7c3926ebc3857b7cecb04
jorgeluis1083 <39818185+jorgeluis1083@users.noreply.github.com>
2018-06-06 23:19:04 -0500

    cargando nueva entrega

---
+++ COMEDOR FINAL/root/ComedorUniversitarioFINAL/application/controllers/reportes/reportes_con.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class reportes_con extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->model("reportes_model");
+	}
+
+	public function index(){
+
+		$fechainicio = $this->input->post("fechainicio");
+		$fechafin = $this->input->post("fechafin");
+
+		$sexo = $this->input->post("sexo");
+		$carrera = $this->input->post("carrera");
+		
+		if ($this->input->post("buscar")) {
+			$resultados = $this->reportes_model->getreportebyDate($fechainicio,$fechafin,$sexo,$carrera);
+		}
+		else{
+			$resultados = $this->reportes_model->getreporte($fechainicio,$fechafin,$sexo,$carrera);
+		}
+		$data = array(
+			"resultados" => $resultados,
+			"sexo" => $sexo,
+			"fechainicio" => $fechainicio,
+			"fechafin" => $fechafin
+		);
+
+		$this->load->view("/header");
+
+		//$this->load->view("reportes/reportes_view",$data);
+		$this->load->view("/reportes/reportes_view",$data);
+		$this->load->view("/footer");
+
+
+	}
+
+
+	public function porcentajevm(){
+
+	
+	
+			$contarvarones= $this->reportes_model->getporcentajevarones();
+
+
+			$contarmujeres = $this->reportes_model->getporcentajemujeres();
+
+			$total=$this->reportes_model->gettotal();;
+
+
+			$data['contarvarones']=$contarvarones;
+			$data['contarmujeres']=$contarmujeres;
+			$data['totales']=$total;
+		
+
+		$this->load->view("/header");
+
+		//$this->load->view("reportes/reportes_view",$data);
+		$this->load->view("/reportes/reportesvm_view",$data);
+		$this->load->view("/footer");
+
+
+	}
+}
